cb46662bdb65dbaa999e222e53c8ea3a28599339
AJ Lauer Barinov <abarinov@mux.com>
2023-09-22 09:00:42 -0700

    feat: AVPlayerItem signed playback URL convenience APIs

--- Tests/MuxAVPlayerSDKTests/PlaybackURLTests.swift
+++ Tests/MuxAVPlayerSDKTests/PlaybackURLTests.swift
@@ -85,4 +85,41 @@ final class PlaybackURLTests: XCTestCase {
             "https://stream.play.example.com/abc.m3u8?token=WhoooopsNotAnActualToken"
         )
     }
+
+    func testCustomDomainPlaybackURL() throws {
+        let playerItem = AVPlayerItem(
+            publicPlaybackID: "abc",
+            customDomain: URL(string: "https://play.example.com")!
+        )
+
+        XCTAssertEqual(
+            (playerItem.asset as! AVURLAsset).url.absoluteString,
+            "https://play.example.com/abc.m3u8"
+        )
+    }
+
+    func testSignedPlaybackURL() throws {
+        let playerItem = AVPlayerItem(
+            signedPlaybackID: "abc",
+            token: "WhoooopsNotAnActualToken"
+        )
+
+        XCTAssertEqual(
+            (playerItem.asset as! AVURLAsset).url.absoluteString,
+            "https://stream.mux.com/abc.m3u8?token=WhoooopsNotAnActualToken"
+        )
+    }
+
+    func testCustomDomainSignedPlaybackURL() throws {
+        let playerItem = AVPlayerItem(
+            signedPlaybackID: "abc",
+            token: "WhoooopsNotAnActualToken",
+            customDomain: URL(string:"https://play.example.com")!
+        )
+
+        XCTAssertEqual(
+            (playerItem.asset as! AVURLAsset).url.absoluteString,
+            "https://play.example.com/abc.m3u8?token=WhoooopsNotAnActualToken"
+        )
+    }
 }
