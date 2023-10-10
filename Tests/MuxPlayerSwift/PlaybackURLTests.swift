432419a4f7f69ca78adce021a8632a0fac24be5d
AJ Lauer Barinov <abarinov@mux.com>
2023-10-09 17:18:20 -0700

    test: verify player item urls

--- Tests/MuxPlayerSwift/PlaybackURLTests.swift
+++ Tests/MuxPlayerSwift/PlaybackURLTests.swift
@@ -19,19 +19,31 @@ final class PlaybackURLTests: XCTestCase {
     }
 
     func testMaximumResolution() throws {
-        let playbackOptions = PlaybackOptions(
-            maximumResolutionTier: .upTo720p
-        )
 
-        let playerItem = AVPlayerItem(
-            playbackID: "abc",
-            playbackOptions: playbackOptions
-        )
+        let expectedURLs: [String: String] = [
+            ResolutionTier.upTo720p.queryValue: "https://stream.mux.com/abc.m3u8?redundant_streams=true&max_resolution=720p",
+            ResolutionTier.upTo1080p.queryValue: "https://stream.mux.com/abc.m3u8?redundant_streams=true&max_resolution=1080p",
+            ResolutionTier.upTo2160p.queryValue: "https://stream.mux.com/abc.m3u8?redundant_streams=true&max_resolution=2160p",
+            ResolutionTier.default.queryValue: "https://stream.mux.com/abc.m3u8?redundant_streams=true",
+        ]
 
-        XCTAssertEqual(
-            (playerItem.asset as! AVURLAsset).url.absoluteString,
-            "https://stream.mux.com/abc.m3u8?redundant_streams=true&max_resolution=720p"
-        )
+        let tiers: [ResolutionTier] = [.upTo720p, .upTo1080p, .upTo2160p, .default]
+
+        for tier in tiers {
+            let playbackOptions = PlaybackOptions(
+                maximumResolutionTier: tier
+            )
+
+            let playerItem = AVPlayerItem(
+                playbackID: "abc",
+                playbackOptions: playbackOptions
+            )
+
+            XCTAssertEqual(
+                (playerItem.asset as! AVURLAsset).url.absoluteString,
+                expectedURLs[tier.queryValue]
+            )
+        }
     }
 
     func testCustomDomainPlaybackURL() throws {
