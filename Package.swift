26c927397bc73740b3dd656c5730cdf9c6527a0d
AJ Lauer Barinov <abarinov@mux.com>
2023-10-26 12:13:36 -0700

    build: bump SDK version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/muxinc/mux-stats-sdk-avplayer",
-            exact: "3.4.1"
+            exact: "3.4.2"
         ),
         .package(
             url: "https://github.com/apple/swift-docc-plugin",
