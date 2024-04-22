fc512e0eb1180c485a29921cfcef7e5ad55b8389
Emily Dixon <edixon@mux.com>
2024-04-22 08:39:58 -0700

    up

--- Sources/MuxPlayerSwift/Fairplay/ContentKeySessionDelegate.swift
+++ Sources/MuxPlayerSwift/Fairplay/ContentKeySessionDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  ContentKeySessionDelegate.swift
-//  
+//
 //
 //  Created by Emily Dixon on 4/19/24.
 //
@@ -66,30 +66,107 @@ class ContentKeySessionDelegate : NSObject, AVContentKeySessionDelegate {
             return
         }
         
-        // step: get app cert (ContentKeyRequest wants to know if this failed)
-        //  the drmtoday example does this synchronously with DispatchGroups, but do we really need to do that?
+        // get app cert
         var applicationCertificate: Data?
+        //  the drmtoday example does this synchronously with DispatchGroups, but do we really need to do that?
         let group = DispatchGroup()
+        group.enter()
         FairplaySessionManager.shared.requestCertificate(
-            playbackID: "", // todo - get through-line from api
-            drmKey: "", // todo - get through-line from api
+            playbackID: "", // todo - get from sdk caller
+            drmKey: "", // todo - get from sdk caller
             completion: { result in
                 if let cert = try? result.get() {
                     applicationCertificate = cert
                 }
-                    
+                group.leave()
             }
         )
+        group.wait()
+        guard let applicationCertificate = applicationCertificate else {
+            print("failed to get application certificate")
+        }
         
+        // step: exchange app cert for SPC using KeyRequest w/completion handler (request wants to know if failed)
         request.makeStreamingContentKeyRequestData(forApp: applicationCertificate,
-                                                      contentIdentifier: assetIDData,
-                                                      options: [AVContentKeyRequestProtocolVersionsKey: [1]],
-                                                      completionHandler: completionHandler)
-        
-        // step: exchange app cert for SPC using KeyRequest w/completion handler
-        
-        // step: exchange SPC for CKC using KeyRequest w/completion handler
+                                                   contentIdentifier: assetIDData,
+                                                   options: [AVContentKeyRequestProtocolVersionsKey: [1]]) { [weak self] spcData, error in
+            guard let self = self else {
+                // todo - log or something?
+                return
+            }
+            
+            guard let spcData = spcData else {
+                print("SPC request failed")
+                // `error` will be non-nil by contract
+                request.processContentKeyResponseError(error!)
+                return
+            }
+            do {
+                // step: exchange SPC for CKC using KeyRequest w/completion handler (request wants to know if failed)
+                try handleContentKeyResponse(spcData: spcData, request: request)
+            } catch {
+                request.processContentKeyResponseError(error)
+            }
+        }
     }
     
-    private func
+    private func handleContentKeyResponse(spcData: Data, request: AVContentKeyRequest)  {
+        do {
+            // Send SPC to Key Server and obtain CKC
+            let asset: AVURLAsset // todo - obtain from sdk caller
+            let playbackID: String // todo - obtain from sdk caller / url of asset
+            
+            // todo - DRM Today example does this synchronously with DispatchGroup. Is that really necessary?
+            let ckcData = try requestContentKeyFromKeySecurityModule(nil, spcData: spcData, playbackID: playbackID, persistable: false)
+            
+            /*
+             AVContentKeyResponse is used to represent the data returned from the key server when requesting a key for
+             decrypting content.
+             */
+            let keyResponse = AVContentKeyResponse(fairPlayStreamingKeyResponseData: ckcData)
+            
+            /*
+             Provide the content key response to make protected content available for processing.
+             */
+            request.processContentKeyResponse(keyResponse)
+        } catch {
+            request.processContentKeyResponseError(error)
+        }
+    }
+    
+    func requestContentKeyFromKeySecurityModule(_ asset: AVURLAsset?, spcData: Data, playbackID: String, persistable: Bool) throws -> Data {
+        
+        // MARK: ADAPT - You must implement this method to request a CKC from your KSM.
+        
+        guard let asset = asset else {
+                    return Data()
+                }
+        
+        var ckcData: Data? = nil
+                
+        let data = try JSONEncoder().encode(asset.stream)
+        let stream = try JSONDecoder().decode(Stream.self, from: data)
+        let token : String? = nil // "{authToken}"
+        
+        let group = DispatchGroup()
+        group.enter()
+        DRMtoday.getLicense(stream: stream, spcData: spcData, token: token, offline: persistable) { (ckc) in
+            ckcData = ckc
+            let escapedName = asset.stream.name.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            let filename = self.getDocumentsDirectory().appendingPathComponent("").appendingPathComponent(escapedName! + (persistable ? "offline" : "online") + ".dat")
+            do {
+                try ckcData?.write(to: filename, options: .atomicWrite)
+            } catch {
+                print("Unable to write a license file")
+            }
+            group.leave()
+        }
+        group.wait()
+        
+        guard ckcData != nil else {
+            throw ProgramError.noCKCReturnedByKSM
+        }
+        
+        return ckcData!
+    }
 }
