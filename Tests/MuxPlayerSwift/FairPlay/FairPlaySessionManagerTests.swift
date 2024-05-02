cb21da110cf7205ab628cf3c1fec9817b8c6a9cd
Emily Dixon <edixon@mux.com>
2024-05-02 14:39:51 -0700

    file for fpssm tests

---
+++ Tests/MuxPlayerSwift/FairPlay/FairPlaySessionManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FairPlaySessionManagerTests.swift
+//  
+//
+//  Created by Emily Dixon on 5/2/24.
+//
+
+import Foundation
+import XCTest
+
+class FairPlaySessionManagerTests : XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+}
