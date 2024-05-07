f2023d2d6f4f71cc2cf376dea6183af581f5f762
Emily Dixon <edixon@mux.com>
2024-05-07 10:47:03 -0700

    setting up

--- Tests/MuxPlayerSwift/FairPlay/ContentKeySessionDelegateTests.swift
+++ Tests/MuxPlayerSwift/FairPlay/ContentKeySessionDelegateTests.swift
@@ -12,9 +12,41 @@ import XCTest
 class ContentKeySessionDelegateTests : XCTestCase {
     
 //    var sessionDelegate: ContentKeySessionDelegate<FairPlayStreamingSessionManager>!
+    var testPlaybackOptionsRegistry: TestPlaybackOptionsRegistry!
+    var testCredentialClient: TestFairPlayStreamingSessionCredentialClient!
+    
+    // object under test
+    var contentKeySessionDelegate: ContentKeySessionDelegate<
+        TestFairPlayStreamingSessionManager
+    >!
     
     override func setUp() async throws {
+        setUpForSuccess()
+    }
+    
+    private func setUpForFailure(error: any Error) {
+        testCredentialClient = TestFairPlayStreamingSessionCredentialClient(
+            failsWith: error
+        )
+        testPlaybackOptionsRegistry = TestPlaybackOptionsRegistry()
         
+        contentKeySessionDelegate = ContentKeySessionDelegate(
+            credentialClient: testCredentialClient,
+            optionsRegistry: testPlaybackOptionsRegistry
+        )
+    }
+    
+    private func setUpForSuccess() {
+        testCredentialClient = TestFairPlayStreamingSessionCredentialClient(
+            fakeCert: "default fake cert".data(using: .utf8)!,
+            fakeLicense: "default fake license".data(using: .utf8)!
+        )
+        testPlaybackOptionsRegistry = TestPlaybackOptionsRegistry()
+        
+        contentKeySessionDelegate = ContentKeySessionDelegate(
+            credentialClient: testCredentialClient,
+            optionsRegistry: testPlaybackOptionsRegistry
+        )
     }
     
     func testParsePlaybackId() throws {
