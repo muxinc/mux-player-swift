f64c27a615782ed2b4614608171eb6b80a267514
AJ Lauer Barinov <abarinov@mux.com>
2024-05-08 11:11:55 -0700

    build: update to AVPlayer Data SDK with privacy manifest

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/muxinc/mux-stats-sdk-avplayer",
-            exact: "3.6.0"
+            exact: "3.6.1"
         ),
         .package(
             url: "https://github.com/apple/swift-docc-plugin",
