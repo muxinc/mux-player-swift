c9c581a1ff810dad3ff958cc842647c851697dd7
AJ Lauer Barinov <abarinov@mux.com>
2024-05-09 20:15:59 -0700

    chore: expose environment variable accessors

--- Examples/MuxPlayerSwiftExample/MuxPlayerSwiftExample/ProcessInfo+EnvironmentVariables.swift
+++ Examples/MuxPlayerSwiftExample/MuxPlayerSwiftExample/ProcessInfo+EnvironmentVariables.swift
@@ -5,6 +5,8 @@
 
 import Foundation
 
+// Configure these as environment variables in
+// application scheme
 extension ProcessInfo {
     var environmentKey: String? {
         guard let value = environment["ENV_KEY"],
@@ -17,6 +19,33 @@ extension ProcessInfo {
 
     var playbackID: String? {
         guard let value = environment["PLAYBACK_ID"],
+                !value.isEmpty else {
+            return nil
+        }
+
+        return value
+    }
+
+    var playbackToken: String? {
+        guard let value = environment["PLAYBACK_TOKEN"],
+                !value.isEmpty else {
+            return nil
+        }
+
+        return value
+    }
+
+    var drmToken: String? {
+        guard let value = environment["DRM_TOKEN"],
+                !value.isEmpty else {
+            return nil
+        }
+
+        return value
+    }
+
+    var customDomain: String? {
+        guard let value = environment["CUSTOM_DOMAIN"],
                 !value.isEmpty else {
             return nil
         }
