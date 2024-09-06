7139cde2d6b1066bc3a71adf98e019af71de70d6
AJ Lauer Barinov <abarinov@mux.com>
2024-09-06 09:27:40 -0700

    this makes more sense here

--- Sources/MuxPlayerSwift/Monitoring/Monitor.swift
+++ Sources/MuxPlayerSwift/Monitoring/Monitor.swift
@@ -115,25 +115,25 @@ class Monitor: ErrorDispatcher {
         bindings[objectIdentifier] = monitoredPlayer
 
         if let player = playerViewController.player {
-            let observation = player.observe(
-                \.error,
-                 options: [.new, .old]
-            ) { player, observation in
-                if let error = (observation.newValue ?? nil) as? NSError,
-                    ((observation.oldValue ?? nil) == nil) {
+            // TODO: Add a better way to protect against
+            // dual registrations for the same player
+            keyValueObservation.unregister(
+                player
+            )
+
+            keyValueObservation.register(
+                player,
+                for: \.error,
+                options: [.new, .old]
+            ) { player, change in
+                if let error = (change.newValue ?? nil) as? NSError,
+                    ((change.oldValue ?? nil) == nil) {
                     binding.dispatchError(
                         "\(error.code)",
                         withMessage: error.localizedFailureReason
                     )
                 }
             }
-
-            if let existingObservation = playersToObservations[ObjectIdentifier(player)] {
-                existingObservation.invalidate()
-                playersToObservations.removeValue(forKey: ObjectIdentifier(player))
-            }
-
-            playersToObservations[ObjectIdentifier(player)] = observation
         }
     }
 
@@ -209,25 +209,25 @@ class Monitor: ErrorDispatcher {
         bindings[objectIdentifier] = monitoredPlayer
 
         if let player = playerLayer.player {
-            let observation = player.observe(
-                \.error,
-                 options: [.new, .old]
-            ) { player, observation in
-                if let error = (observation.newValue ?? nil) as? NSError,
-                    ((observation.oldValue ?? nil) == nil) {
+            // TODO: Add a better way to protect against
+            // dual registrations for the same player
+            keyValueObservation.unregister(
+                player
+            )
+
+            keyValueObservation.register(
+                player,
+                for: \.error,
+                options: [.new, .old]
+            ) { player, change in
+                if let error = (change.newValue ?? nil) as? NSError,
+                    ((change.oldValue ?? nil) == nil) {
                     binding.dispatchError(
                         "\(error.code)",
                         withMessage: error.localizedFailureReason
                     )
                 }
             }
-
-            if let existingObservation = playersToObservations[ObjectIdentifier(player)] {
-                existingObservation.invalidate()
-                playersToObservations.removeValue(forKey: ObjectIdentifier(player))
-            }
-
-            playersToObservations[ObjectIdentifier(player)] = observation
         }
     }
 
@@ -276,5 +276,38 @@ class Monitor: ErrorDispatcher {
         playbackID: String
     ) {
 
+    class KeyValueObservation {
+        var observations: [ObjectIdentifier: Set<NSKeyValueObservation>] = [:]
+
+        func register<Value>(
+            _ player: AVPlayer,
+            for keyPath: KeyPath<AVPlayer, Value>,
+            options: NSKeyValueObservingOptions,
+            changeHandler: @escaping (AVPlayer, NSKeyValueObservedChange<Value>) -> Void
+        ) {
+            let observation = player.observe(
+                keyPath,
+                options: options,
+                changeHandler: changeHandler
+            )
+
+            if var o = observations[ObjectIdentifier(player)] {
+                o.insert(observation)
+                observations[ObjectIdentifier(player)] = o
+            } else {
+                observations[ObjectIdentifier(player)] = Set(arrayLiteral: observation)
+            }
+        }
+
+        func unregister(
+            _ player: AVPlayer
+        ) {
+            if let o = observations[ObjectIdentifier(player)] {
+                o.forEach { observation in
+                    observation.invalidate()
+                }
+                observations.removeValue(forKey: ObjectIdentifier(player))
+            }
+        }
     }
 }
