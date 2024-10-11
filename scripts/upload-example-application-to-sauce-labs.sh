e2fa5adafbb7d57bab81dcb881d682bdecff4d36
AJ Lauer Barinov <abarinov@mux.com>
2024-10-11 13:43:44 -0700

    use raw jq output

--- scripts/upload-example-application-to-sauce-labs.sh
+++ scripts/upload-example-application-to-sauce-labs.sh
@@ -28,7 +28,7 @@ export SAUCE_ACCESS_KEY=$BUILDKITE_MAC_STADIUM_SAUCE_ACCESS_KEY
 
 echo "▸ Uploading test application to Sauce Labs App Storage"
 
-app_file_id=$(curl -s -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" --form "name=\"${APPLICATION_NAME}\"" | jq '.item.id')
+app_file_id=$(curl -s -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" --form "name=\"${APPLICATION_NAME}\"" | jq -r '.item.id')
 
 if [[ $? == 0 ]]; then
     echo "▸ Successfully uploaded to Sauce Labs application storage. File ID: $app_file_id"
