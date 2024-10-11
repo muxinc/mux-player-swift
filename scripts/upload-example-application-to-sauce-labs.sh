9dcc6e58df7c237ed0f8c49099684309e8b857cb
AJ Lauer Barinov <abarinov@mux.com>
2024-10-11 13:23:36 -0700

    quotes

--- scripts/upload-example-application-to-sauce-labs.sh
+++ scripts/upload-example-application-to-sauce-labs.sh
@@ -25,7 +25,7 @@ echo "▸ Uploading test application to Sauce Labs App Storage"
 app_file_id= curl -s -u "$SAUCE_USER_NAME:$SAUCE_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
 --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" \
---form "name=\"${APPLICATION_NAME}\"" | jq `.item.id`
+--form "name=\"${APPLICATION_NAME}\"" | jq '.item.id'
 
 if [[ $? == 0 ]]; then
     echo "▸ Successfully uploaded to Sauce Labs application storage. File ID: $app_file_id"
