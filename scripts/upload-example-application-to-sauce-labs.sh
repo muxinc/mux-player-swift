afe444251730070a28c906ce83e9e3785b2bca5f
AJ Lauer Barinov <abarinov@mux.com>
2024-10-11 11:09:11 -0700

    ci: remove dry-run curl and do the real deal curl to upload example app to SL

--- scripts/upload-example-application-to-sauce-labs.sh
+++ scripts/upload-example-application-to-sauce-labs.sh
@@ -16,21 +16,11 @@ if [ ! -f $APPLICATION_PAYLOAD_PATH ]; then
     echo -e "\033[1;31m ERROR: application archive not found \033[0m"
 fi
 
-# TODO: Fetch these
-export SAUCE_USERNAME=""
-export SAUCE_ACCESS_KEY=""
+
 
 echo "▸ Uploading test application to Sauce Labs App Storage"
 
-# This curl command is a dry-run. Remove redirect to localhost after SL credentials are available
-# Non-dry-run command
-#  curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
-# --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
-# --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" \
-# --form "name=\"${APPLICATION_NAME}\""
-
-curl --resolve '*:80:127.0.0.1' --resolve '*:443:127.0.0.1' 'https://api.us-west-1.saucelabs.com' \
--u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
+curl -v -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
 --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" \
 --form "name=\"${APPLICATION_NAME}\""
