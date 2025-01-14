492b90efbe2363c9f689233ffc6d54b77b558eb6
Emily Dixon <edixon@mux.com>
2025-01-14 13:33:51 -0800

    use sauce creds

--- scripts/run-sauce-tests.sh
+++ scripts/run-sauce-tests.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
+# TODO - Add this section to the mac mini buildkite env
+readonly SAUCE_USERNAME=${BUILDKITE_MAC_STADIUM_SAUCE_USERNAME}
+readonly SAUCE_ACCESS_KEY=${BUILDKITE_MAC_STADIUM_SAUCE_ACCESS_KEY}
+
+brew tap saucelabs/saucectl
+brew install saucectl
+
 if [ -z ${BUILDKITE_BUILD} && -z ${BUILDKITE_BUILD} ] 
   then
     readonly BUILD_LABEL="${BUILDKITE_BUILD}-${BUILDKIE_BRANCH}"
   else 
-    readonly BUILD_LABEL="Local Build"
+    readonly BUILD_LABEL="dev"
 fi
 
-scripts/create-example-application-archive.sh MuxPlayerSwiftExample && saucectl run --build ${BUILD_LABEL}
+scripts/create-example-application-archive.sh MuxPlayerSwiftExample && saucectl run --build "${BUILD_LABEL}"
 
