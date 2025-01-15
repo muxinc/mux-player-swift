c8a9d7f5434db3e415e84aa6d3c4469192f47c29
AJ Lauer Barinov <abarinov@mux.com>
2025-01-14 17:17:02 -0800

    ci: remove curl commands to separately upload example app
    this already done by sauce labs

--- scripts/upload-example-application-to-sauce-labs.sh
+++ scripts/upload-example-application-to-sauce-labs.sh
@@ -26,21 +26,6 @@ fi
 export SAUCE_USERNAME=$BUILDKITE_MAC_STADIUM_SAUCE_USERNAME
 export SAUCE_ACCESS_KEY=$BUILDKITE_MAC_STADIUM_SAUCE_ACCESS_KEY
 
-# saucectl does these parts for us, we don't need to curl anything
-#echo "▸ Uploading test application to Sauce Labs App Storage"
-
-#curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' --form "payload=@\"${APPLICATION_PAYLOAD_PATH}\"" --form "name=\"${APPLICATION_NAME}\""
-
-#if [[ $? == 0 ]]; then
-#    echo "▸ Successfully uploaded to Sauce Labs application storage."
-#else
-#    echo -e "\033[1;31m ERROR: Failed to upload to Sauce Labs application storage. Check for valid credentials. \033[0m"
-#    exit 1
-#fi
-
-#echo "▸ Deploying tests to Sauce Labs"
-
-
 echo "▸ Deploying app and Testing with Sauce"
 echo "▸ Sauce Labs config: $(cat $PWD/.sauce/config.yml)"
 saucectl run -c "$PWD/.sauce/config.yml"
