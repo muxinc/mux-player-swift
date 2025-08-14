edf4024281a44d9c1c60697deb30f07238d6b128
Emily Dixon <edixon@mux.com>
2025-08-14 11:59:20 -0700

    headerdoc

--- Sources/MuxPlayerSwift/PublicAPI/Extensions/AVPlayerItem+Mux.swift
+++ Sources/MuxPlayerSwift/PublicAPI/Extensions/AVPlayerItem+Mux.swift
@@ -10,16 +10,28 @@ import AVKit
 
 public extension AVPlayerItem {
     
-    // Initializes a player item with a playback URL that
-    // references your Mux Video at the supplied playback ID.
-    // The playback ID must be public.
-    //
-    // This initializer uses https://stream.mux.com as the
-    // base URL. Use a different initializer if using a custom
-    // playback URL.
-    //
-    // - Parameter playbackID: playback ID of the Mux Asset
-    // you'd like to play
+    /// Initializes a player item with a playback URL that
+    /// references your Mux Video at the supplied playback ID.
+    /// The playback ID must be public.
+    ///
+    /// This initializer uses https://stream.mux.com as the
+    /// base URL. Use a different initializer if using a custom
+    /// playback URL.
+    ///
+    /// - Parameter playbackID: playback ID of the Mux Asset
+    /// you'd like to play
+    ///
+    /// > important: To use Mux Data, you must still  use ``AVPlayerViewController.prepare(playbackID:)``, ``AVPlayerLayer.prepare(playbackID:)`` or a realted method
+    ///
+    /// - SeeAlso:
+    ///   -  ``AVPlayerViewController.init(playbackID:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:monitoringOptions:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:playbackOptions:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:playbackOptions:monitoringOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:monitoringOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:playbackOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:playbackOptions:monitoringOptions:)``
     convenience init(playbackID: String) {
         self.init(
             playbackID: playbackID,
@@ -28,13 +40,27 @@ public extension AVPlayerItem {
         )
     }
 
-    // Initializes a player item with a playback URL that
-    // references your Mux Video at the supplied playback ID.
-    // The playback ID must be public.
-    //
-    // - Parameters:
-    //   - playbackID: playback ID of the Mux Asset
-    //   you'd like to play
+    /// Initializes a player item with a playback URL that
+    /// references your Mux Video at the supplied playback ID.
+    /// The playback ID must be public.
+    ///
+    /// - Parameters:
+    ///   - playbackID: playback ID of the Mux Asset
+    ///   you'd like to play
+    ///   - playbackOptions: Options for how to play your asset
+    /// > important: To use Mux Data, you must still  use ``AVPlayerViewController.prepare(playbackID:)``, ``AVPlayerLayer.prepare(playbackID:)`` or a realted method
+    ///
+    /// - SeeAlso:
+    ///   -  ``AVPlayerViewController.init(playbackID:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:monitoringOptions:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:playbackOptions:)``
+    ///   -  ``AVPlayerViewController.init(playbackID:playbackOptions:monitoringOptions:)``
+    ///   -  ``AVPlayerViewController.prepare(playbackID:playbackOptions:monitoringOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:monitoringOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:playbackOptions:)``
+    ///   -  ``AVPlayerLayer.init(playbackID:playbackOptions:monitoringOptions:)``
+    ///   -  ``AVPlayerLayer.prepare(playbackID:playbackOptions:monitoringOptions:)``
     convenience init(
         playbackID: String,
         playbackOptions: PlaybackOptions
@@ -49,7 +75,7 @@ public extension AVPlayerItem {
 
 public extension AVPlayerItem {
     
-    // Extracts Mux playback ID from remote AVAsset, if possible
+    /// Extracts Mux playback ID from remote AVAsset, if possible
     var playbackID: String? {
         guard let remoteAsset = asset as? AVURLAsset else {
             return nil
