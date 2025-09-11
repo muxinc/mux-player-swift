e06aa3c13949530a0892af0d5ed9791b5bb0dc52
Emily Dixon <edixon@mux.com>
2025-09-11 14:19:26 -0700

    Automatic Error Tracking

--- Sources/MuxPlayerSwift/PublicAPI/Options/MonitoringOptions.swift
+++ Sources/MuxPlayerSwift/PublicAPI/Options/MonitoringOptions.swift
@@ -16,14 +16,20 @@ public struct MonitoringOptions {
     /// monitoring with Mux Data
     public var playerName: String
 
+    /// Track player errors automatically using Mux Data.
+    /// If you disable automatic error tracking, you can track errors using methods on ``MUXSDKStats``
+    public var automaticErrorTracking: Bool
+    
     var customerData: MUXSDKCustomerData?
 
     /// Initializes options to customize monitoring by Mux
     /// - Parameter playbackID: helps identify your Data environment,
-    public init(playbackID: String) {
+    /// - Parameter automaticErrorTracking: Track errors automatically in Mux Data. Default is true
+    public init(playbackID: String, automaticErrorTracking: Bool = true) {
         self.environmentKey = nil
         let uniquePlayerName = "\(playbackID)-\(UUID().uuidString)"
         self.playerName = uniquePlayerName
+        self.automaticErrorTracking = automaticErrorTracking
     }
 
     /// Initializes options to customize monitoring by Mux
@@ -31,9 +37,11 @@ public struct MonitoringOptions {
     ///   - environmentKey: identifies your Data environment,
     ///   generated in the Dashboard
     ///   - playerName: identifier of the player
-    public init(environmentKey: String, playerName: String) {
+    ///   - automaticErrorTracking: Track errors in automatically in Mux Data. Default is true
+    public init(environmentKey: String, playerName: String, automaticErrorTracking: Bool = true) {
         self.environmentKey = environmentKey
         self.playerName = playerName
+        self.automaticErrorTracking = automaticErrorTracking
     }
 
     /// Initializes options to customize monitoring by Mux
@@ -42,12 +50,15 @@ public struct MonitoringOptions {
     ///   - customerData: passed through as-is when initializing
     ///   Mux Data monitoring
     ///   - playerName: identifier of the player
+    ///   - automaticErrorTracking: Track errors automatically in Mux Data. Default is true
     public init(
         customerData: MUXSDKCustomerData,
-        playerName: String
+        playerName: String,
+        automaticErrorTracking: Bool = true
     ) {
         self.customerData = customerData
         self.environmentKey = nil
         self.playerName = playerName
+        self.automaticErrorTracking = automaticErrorTracking
     }
 }
