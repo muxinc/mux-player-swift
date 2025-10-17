cecc914b8355419523cb28e4aa09435c5d1a9e05
Emily Dixon <edixon@mux.com>
2025-10-17 12:36:14 -0700

    Ok here's the first part

--- Sources/MuxPlayerSwift/InternalExtensions/MuxPlayerContext.swift
+++ Sources/MuxPlayerSwift/InternalExtensions/MuxPlayerContext.swift
@@ -4,13 +4,11 @@ import AVFoundation
 import UIKit
 import MUXSDKStats
 
-// TODO: Should this be an actor? that provides isolation, but makes it annoying to access the player. @MainActor might be enough since AVPlayer is also annotated that way
-// purpose of this class is to manage player binding and DRM connection
+// Connect a PlayerBinding to Player (and UI object, as required by Data SDK)
 //  - can be used in a container VC along with a VC in order to manage there (create new context when VC player is assigned)
 //  - can be used as an associated object with our extensions instead of the dictionary maze we currently have
-//  - (in the future, when VC/View no longer needed) can be used in a SwiftUI view as a state object to contain the player
+//  - (in the future, when VC/View no longer needed) can be used in a SwiftUI view as a state object to contain the player/playerbinding
 class MuxPlayerContext {
-    // TOOD: Wait make this optional and need to unbind() whenever this is cleared
     public let player: AVPlayer
     
     private var monitoringInfo: MonitoringInfo?
@@ -47,7 +45,7 @@ class MuxPlayerContext {
         customerData: MUXSDKCustomerData = MUXSDKCustomerData(),
         automaticErrorTracking: Bool = true
     ) {
-        // TODO: start monitoring, but don't hold a reference to the view
+        layer.player = self.player
         endMonitoring()
         
         let binding = MUXSDKStats.monitorAVPlayerLayer(
@@ -76,7 +74,6 @@ class MuxPlayerContext {
     
     func unbind() {
         endMonitoring()
-        // TODO: Remove Asset from DRM Session if present
         player.replaceCurrentItem(with: nil)
     }
     
@@ -86,9 +83,6 @@ class MuxPlayerContext {
     
     init(player: AVPlayer) {
         self.player = player;
-        
-        // TODO: Observe Player for PlayerItem changes? Then you're subject to notification ordering and stuff, but maybe doesn't matter
-        //  we need the player item to restart playback after a media services reset and in unbind()
     }
     
     deinit {
