fc26d693cc2574f0fa14595024d9159db9d399df
Emily Dixon <edixon@mux.com>
2026-04-24 12:14:38 -0700

    Ok use let in prod

--- Sources/MuxPlayerSwift/Lifecycle/PlayerSDK.swift
+++ Sources/MuxPlayerSwift/Lifecycle/PlayerSDK.swift
@@ -9,7 +9,11 @@ import os
 
 // internal class to manage dependency injection
 class PlayerSDK {
+    #if DEBUG
     static var shared = PlayerSDK()
+    #else
+    static let shared = PlayerSDK()
+    #endif
 
     var monitor: Monitor
 
