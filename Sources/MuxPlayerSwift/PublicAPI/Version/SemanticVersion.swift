f2f9ede770734ff8510bdb87d894b90c12841d5f
Eugene Morozov <emorozov@mux.com>
2026-07-13 16:13:19 -0700

    Version Bump

--- Sources/MuxPlayerSwift/PublicAPI/Version/SemanticVersion.swift
+++ Sources/MuxPlayerSwift/PublicAPI/Version/SemanticVersion.swift
@@ -11,7 +11,7 @@ public struct SemanticVersion {
     public static let major = 1
 
     /// Minor version component.
-    public static let minor = 7
+    public static let minor = 8
 
     /// Patch version component.
     public static let patch = 0
